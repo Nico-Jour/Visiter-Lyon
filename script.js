774673a9fb17500e0db2a51865566d999645cffd
Lucas <90116481+LucasRibeiro19@users.noreply.github.com>
2021-12-20 22:46:53 +0100

    added menu burger

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const ctaBurger = document.querySelector('.cta__burger');
+const menuBurger = document.querySelector('.menu__burger');
 
 ctaBurger.addEventListener('click', ()=> {
     ctaBurger.classList.toggle('open')
+    menuBurger.classList.toggle('active')
 })
