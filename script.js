335ace30ee7f7b2a8d843c8dc2f748c18d582246
Lucas <90116481+LucasRibeiro19@users.noreply.github.com>
2022-01-13 20:22:27 +0100

    Homepage header fixed

--- script.js
+++ script.js
@@ -3,10 +3,5 @@ const menuBurger = document.querySelector('.menu__burger');
 
 ctaBurger.addEventListener('click', ()=> {
     ctaBurger.classList.toggle('open')
-<<<<<<< HEAD
-})
-
-=======
     menuBurger.classList.toggle('active')
 })
->>>>>>> origin/main
